0d5a2e9fd949d5c504792bdb2d9804fb27aab8dc
chinklaus <chinklaus@gmail.com>
2020-07-17 14:57:21 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,8 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          Welcome to <a href="/">Somatt!</a>
+        </h1>        
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
@@ -48,15 +44,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+      <footer>    
       </footer>
 
       <style jsx>{`
